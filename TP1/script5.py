0eed768a2bad133523a907353eb906ebe732c83a
NNavall <niconavall@gmail.com>
2021-04-24 16:35:09 -0300

    Hice 7, terminar de redondear el resto

--- TP1/script5.py
+++ TP1/script5.py
@@ -89,11 +89,11 @@ def ej6():
     valoresc=[0.5, 1, 1.5, 2, 2.5]
     for c in valoresc:
         valores_guardados=[]
-        subprocess.check_output("./TP0.out b 10000 5 "+str(c), shell=True, universal_newlines=True)
+        subprocess.check_output("./TP0.out a 10000 5 "+str(c), shell=True, universal_newlines=True)
         os.rename('ej.data', 'ej.test')
         for i in range(0,20):
             #estoy corriendo python 2 aca
-            subprocess.check_output("./TP0.out b 250 5 "+str(c), shell=True, universal_newlines=True)
+            subprocess.check_output("./TP0.out a 250 5 "+str(c), shell=True, universal_newlines=True)
             output= subprocess.check_output("./c4.5 -f ej -g -u", shell=True, universal_newlines=True)
             valores_guardados.append(get_training_and_testing_values(output))
 
@@ -114,4 +114,100 @@ def ej6():
 
     f.close()
 
-ej6()
+
+def get_values_data(s,n):
+    listofstrings = re.split('\n', s)
+    #print(listofstrings)
+    print(re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", listofstrings[n]))
+
+def ej6_1():
+    f = open("ej6-1.txt", "a")
+
+    f.write("CValue    ErrorMinimo\n")
+
+    def predict_parallel(coord):
+        if (float(coord[0])>0):
+            return 1
+        else:
+            return 0
+
+    def predict_diagonal(coord):
+        #if (-float(coord[0])<float(coord[1]) and -float(coord[0])<float(coord[2]) and -float(coord[0])<float(coord[3]) and -float(coord[0])<float(coord[4])):
+        #if (-float(coord[0])<float(coord[1]) or -float(coord[0])<float(coord[2]) or -float(coord[0])<float(coord[3]) or -float(coord[0])<float(coord[4])):
+        if ((-float(coord[0]))<float(coord[1])):
+            return 1  #etiqueto 1, pero tengo que ver que pasa con las otras componentes si comparo con coord[0]
+        else:
+            return 0
+
+    valoresc=[0.5, 1, 1.5, 2, 2.5]
+    for c in valoresc:
+        correct=0
+        subprocess.check_output("./TP0.out a 10000 5 "+str(c), shell=True, universal_newlines=True)
+        os.rename('ej.data', 'minnimunerror.data')
+
+        file = open('minnimunerror.data', 'r')
+        
+        for i in range(0,10000):
+            valores_leidos=re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", file.readline())
+            
+            #Si estamos en ejercicio a)diagonal
+            if (float(valores_leidos[5])==predict_diagonal(valores_leidos[:5])):#fijarse si estoy haciendolo bien
+                correct+=1
+            
+
+            #Si estamos en ejercicio b)paralelo
+            """
+            if (float(valores_leidos[5])==predict_parallel(valores_leidos[:5])):#fijarse si estoy haciendolo bien
+                correct+=1
+                #print(str(valores_leidos[5])+"=="+str(predict_parallel(valores_leidos[:3])))
+            """
+        
+        file.close()
+        
+        #print(correct)
+        #print(float(correct)/10000)
+
+        f.write(str(c) + "    " + str(100-float(correct)/100) + "\n")
+
+        time.sleep(1)
+
+
+
+    f.close()
+
+def ej7():
+    f = open("ej7.txt", "a")
+
+    f.write("Dimensiones    TrainingBeforePrunningError    TrainingAfterPrunningError    TestingBeforePrunningError    TestingAfterPrunningError\n")
+
+    valoresd=[2, 4, 8, 16, 32]
+    for d in valoresd:
+        valores_guardados=[]
+        subprocess.check_output("./TP0.out b 10000 "+str(d)+" 0.78", shell=True, universal_newlines=True)
+        os.rename('ej.data', 'ej.test')
+        for i in range(0,20):
+            #estoy corriendo python 2 aca
+            subprocess.check_output("./TP0.out b 250 "+str(d)+" 0.78", shell=True, universal_newlines=True)
+            output= subprocess.check_output("./c4.5 -f ej -g -u", shell=True, universal_newlines=True)
+            valores_guardados.append(get_training_and_testing_values(output))
+
+        #BeforePruningSize, BeforePruningErrors, BeforePruningErrors%, AfterPruningSize, AfterPruningErrors, AfterPruningErrors%, Estimate%]
+
+        trainingBeforePError=reduce(sum,map(lambda x: x[0][2],valores_guardados))/len(valores_guardados)
+        trainingAfterPError=reduce(sum,map(lambda x: x[0][5],valores_guardados))/len(valores_guardados)
+
+        testingBeforePError=reduce(sum,map(lambda x: x[1][2],valores_guardados))/len(valores_guardados)
+        testingAfterPError=reduce(sum,map(lambda x: x[1][5],valores_guardados))/len(valores_guardados)
+
+        
+        f.write(str(d) + "    " + str(trainingAfterPError) + "    " + str(trainingAfterPError) + "    " + str(testingBeforePError) + "    " + str(testingAfterPError) + "\n")
+
+        time.sleep(1)
+
+
+
+    f.close()
+
+#ej6()
+#ej6_1()
+ej7()
