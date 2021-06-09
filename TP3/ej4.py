a7a9ae3f2e6c5f59d624d3a24c60de581ebf5035
NNavall <niconavall@gmail.com>
2021-06-09 20:08:57 -0300

    Termine tp3

--- TP3/ej4.py
+++ TP3/ej4.py
@@ -8,23 +8,23 @@ def get_values(s):
 
 def ej4(nombreDatos,bins):
     valores=[]
-    min=2
-    for n in range(2,bins+1):
+    min=1
+    for n in range(1,bins):
         output= subprocess.check_output("./nb_n_4.out "+nombreDatos+" "+str(n), shell=True, universal_newlines=True)
         os.rename(nombreDatos+".predic", nombreDatos+"-"+str(n)+".predic")
         #print(output)
         s= output.split('Errores:', 1)[1]
         valores.append(get_values(s))
         #print(n)
-        if valores[n-2][1]<valores[min-2][1]: #comparo errores de validacion
+        if valores[n-1][1]<valores[min-1][1]: #comparo errores de validacion
             min=n
 
     os.rename(nombreDatos+"-"+str(min)+".predic", nombreDatos+".predic")
-    for n in range(2,bins+1):
+    for n in range(1,bins):
         if n != min: #para evitar que intente eliminar al que cambie de nombre
             os.remove(nombreDatos+"-"+str(n)+".predic")
 
-    print("Bins optimos: "+str(min)+"\nEntrenamiento:"+str(valores[min-2][0])+"%\nValidacion:"+str(valores[min-2][1])+"%\nTest:"+str(valores[min-2][2])+"%")
+    print("Bin optimo: "+str(min)+"\nEntrenamiento:"+str(valores[min-2][0])+"%\nValidacion:"+str(valores[min-2][1])+"%\nTest:"+str(valores[min-2][2])+"%")
 
     f = open("ej4.txt", "w")
     f.write("ErrorEntrenamiento ErrorValidacion ErrorTest\n")
@@ -43,14 +43,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-"""
-def test():
-    output= subprocess.check_output("./nb_n_4.out test_diagonal 6", shell=True, universal_newlines=True)
-    f = open("test_diagonal.txt", "w")
-    f.write(output)
-    f.close()
-
-test()
-"""
 
